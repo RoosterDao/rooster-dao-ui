e8ca03522f08464befdd52ecd67c9c1bc7a1f683
Wouter Godefroy <wgo@belitas.be>
2022-06-19 11:10:03 +0200

    added mock read functions

--- contracts/governor/lib.rs
+++ contracts/governor/lib.rs
@@ -58,6 +58,62 @@ mod governor {
             let caller = Self::env().caller();
             self.proposals.get(&caller).unwrap_or_default()
         }
+
+        #[ink(message)]
+        pub fn get_votes(&self, account: AccountId, blocknumber: BlockNumber) -> u32 {
+            ink_env::debug_println!("getVotes: account={:?} blocknumber={:?}", account, blocknumber);
+            0
+        }
+
+        #[ink(message)]
+        pub fn has_voted(&self, proposal_id: u128, account: AccountId) -> bool {
+            ink_env::debug_println!("has_voted: proposal_id={}, account={:?}",proposal_id, account);
+            false
+        }
+
+        #[ink(message)]
+        pub fn name(&self) -> String {
+            ink_env::debug_println!("name");
+            "Governer".to_string()
+        }
+
+        #[ink(message)]
+        pub fn proposal_deadline(&self, proposal_id: u128) -> BlockNumber {
+            ink_env::debug_println!("proposal_deadline: proposal_id={}", proposal_id);
+            ink_env::block_number::<ink_env::DefaultEnvironment>()
+        }
+
+        #[ink(message)]        
+        pub fn proposal_snapshot(&self, proposal_id: u128) -> BlockNumber {
+            ink_env::debug_println!("proposal_snapshot: proposal_id={}", proposal_id);
+            ink_env::block_number::<ink_env::DefaultEnvironment>()
+        }
+
+        #[ink(message)]
+        pub fn proposal_votes(&self, proposal_id: u128) -> u32 {
+            ink_env::debug_println!("proposal_votes: proposal_id={}", proposal_id);
+            0
+        }
+
+        #[ink(message)]
+        pub fn state(&self, proposal_id: u128) -> bool {
+            ink_env::debug_println!("state: proposal_id={}", proposal_id);
+            false
+        }
+
+        #[ink(message)]
+        pub fn voting_delay(&self) -> u32 {
+            ink_env::debug_println!("voting_delay");
+            0
+        }
+
+        #[ink(message)]
+        pub fn voting_period(&self) -> u32 {
+            ink_env::debug_println!("voting_period");
+            0
+        }
+
+
     }
 
     #[cfg(test)]
@@ -76,7 +132,68 @@ mod governor {
             let mut governor = Governor::new();
             let prop_msg = "hello world!";
             governor.propose(prop_msg.to_string());
-            assert_eq!(governor.get_own_proposal().proposal,prop_msg)
+            assert_eq!(governor.get_own_proposal().proposal,prop_msg);
         }
+
+        #[ink::test]
+        fn get_votes_works() {
+            let governor = Governor::new();
+            let block_number = ink_env::block_number::<ink_env::DefaultEnvironment>();
+            let accounts =
+                ink_env::test::default_accounts::<ink_env::DefaultEnvironment>();
+            assert_eq!(governor.get_votes(accounts.alice, block_number),0);
+        }
+
+        #[ink::test]
+        fn has_voted_works() {
+            let governor = Governor::new();
+            let accounts =
+                ink_env::test::default_accounts::<ink_env::DefaultEnvironment>();
+            assert_eq!(governor.has_voted(0, accounts.alice), false);
+        }
+
+        #[ink::test]
+        fn name_works() {
+            let governor = Governor::new();
+            assert_eq!(governor.name(), "Governer");
+        }
+
+        #[ink::test]
+        fn proposal_deadline_works() {
+            let governor = Governor::new();
+            assert_eq!(governor.proposal_deadline(0),ink_env::block_number::<ink_env::DefaultEnvironment>());
+        }
+
+        #[ink::test]
+        fn proposal_snapshot_works() {
+            let governor = Governor::new();
+            assert_eq!(governor.proposal_snapshot(0),ink_env::block_number::<ink_env::DefaultEnvironment>());
+        }
+
+        #[ink::test]
+        fn proposal_votes_works() {
+            let governor = Governor::new();
+            assert_eq!(governor.proposal_votes(0), 0);
+        }
+
+        #[ink::test]
+        fn state_works() {
+            let governor = Governor::new();
+            assert_eq!(governor.state(0), false);
+        }
+
+        #[ink::test]
+        fn voting_delay_works() {
+            let governor = Governor::new();
+            assert_eq!(governor.voting_delay(),0);
+        }
+
+        #[ink::test]
+        fn voting_period_works() {
+            let governor = Governor::new();
+            assert_eq!(governor.voting_period(),0);
+        }
+
+
     }
 }
